abe8e8542cf86167fb2bae4d4c2a66bf29e19f5a
titronius <77384832+titronius@users.noreply.github.com>
2024-07-12 09:42:54 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-bot_token = 'vk1.a.t7G6yNaLkSaXXAgWXL6gOJKYbZnaherGwtA37VliYK5YhxiVy2XgTewIc41ekt-A8NH9ce7_lGsvR8DcQwcwI0p9W9rGa544s8Zxd8_P_xQjzGWxheD3iHoO6ukmxrFEdIVDTsli1KAWYzzvRskZP2EhYn-mLcnf45tjgkIYmyalvimHZvZBt3cjfzArL5pxZBB1MyI8seCSXm5MRzzVUA'
+bot_token = 'token'
 
 greeting_msg = """
 Доброго дня суток!
