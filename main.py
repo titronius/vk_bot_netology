62f6c9716f742aaad1f06966a9b8ce97a0439484
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2024-07-12 19:51:03 +0500

    changed main.py

--- main.py
+++ main.py
@@ -1,66 +1,155 @@
-from random import randrange
-
-import settings
 import json
+from random import randrange
+from pprint import pprint
+from pathlib import Path
+
 import vk_api
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-# from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-vk_session = vk_api.VkApi(token=settings.bot_token)
-vk = vk_session.get_api()
-longpoll = VkBotLongPoll(vk_session, group_id=226538050)
+from models import create_tables, drop_tables
+from vk_interaction import User, VKSession
 
-CALLBACK_TYPES = ('show_snackbar', 'open_link', 'open_app')
 
-for event in longpoll.listen():
-    print(event)
-    if event.type == VkBotEventType.MESSAGE_NEW:
-        if event.object.message['text'] != "":
-            request = event.object.message['text']
+def get_env_variable(variable_name):
+    """
+    Получение значения переменной окружения из файла .env.
 
-            if request == "Начать":
-                keyboard = VkKeyboard()
-                keyboard.add_button(settings.buttons_for_bot[0], color=VkKeyboardColor.SECONDARY)
-                keyboard.add_button(settings.buttons_for_bot[1], color=VkKeyboardColor.SECONDARY)
-                vk.messages.send(user_id = event.object.message['from_id'], random_id = get_random_id(), keyboard = keyboard.get_keyboard(), message = settings.greeting_msg)
-            
-            elif request == settings.buttons_for_bot[0]:
-                # - имя и фамилия,
-                # - ссылка на профиль,
-                # - три фотографии в виде attachment(https://dev.vk.com/method/messages.send).
-                msg = 'Красотка Красивая'
-                link = 'https://vk.com/titronius'
-                attachment = "photo12908812_457240175,photo12908812_457240168"
-                
-                keyboard = VkKeyboard(inline = True)
-                keyboard.add_openlink_button(label = '🔗 Ссылка на профиль', link = link)
-                keyboard.add_line()
-                keyboard.add_callback_button(label = '❤️ Добавить в избранное', color=VkKeyboardColor.SECONDARY, payload = {"type": "add_to_favorite"})
-                keyboard.add_callback_button(label = '❌ Добавить в чс', color=VkKeyboardColor.SECONDARY, payload = {"type": "add_to_blacklist"})
-                keyboard.add_line()
-                keyboard.add_callback_button(label = '➡️ Следующий', color=VkKeyboardColor.SECONDARY, payload = {"type": "next_people"})
-                
-                vk.messages.send(user_id = event.object.message['from_id'], random_id = get_random_id(), keyboard = keyboard.get_keyboard(), message = msg, attachment = attachment)
+    Args:
+        variable_name (str): Название переменной окружения.
 
-            elif request == settings.buttons_for_bot[1]:
-                # Вывести список избранных людей.
-                pass
-    elif event.type == VkBotEventType.MESSAGE_EDIT:
+    Returns:
+        str: Значение переменной окружения.
+    
+    Raises:
+        ValueError: Если переменная окружения не найдена.
+    """
+    env_path = Path('.').joinpath('.env')
+    env_text = Path(env_path).read_text()
 
-        if event.object.payload.get('type') in CALLBACK_TYPES:
-            # отправляем серверу указания как какую из кнопок обработать. Это заложено в 
-            # payload каждой callback-кнопки при ее создании.
-            # Но можно сделать иначе: в payload положить свои собственные
-            # идентификаторы кнопок, а здесь по ним определить
-            # какой запрос надо послать. Реализован первый вариант.
-            r = vk.messages.sendMessageEventAnswer(
-                        event_id=event.object.event_id,
-                        user_id=event.object.user_id,
-                        peer_id=event.object.peer_id,                                                   
-                        event_data=json.dumps(event.object.payload))
+    for line in env_text.splitlines():
+        if line.startswith(variable_name):
+            return line.split('=')[1].strip()
+    
+    raise ValueError(f"{variable_name} не задан в переменных окружения")
+
+class VKBot:
+    def __init__(self, vk_session, group_id):
+        self.vk_session = vk_session
+        self.vk = vk_session.get_api()
+        self.longpoll = VkBotLongPoll(vk_session, group_id=group_id)
+        self.callback_types = ('show_snackbar', 'open_link', 'open_app')
+        
+    def send_message(self, user_id, message, keyboard=None, attachment=None):
+        self.vk.messages.send(
+            user_id=user_id,
+            random_id=get_random_id(),
+            message=message,
+            keyboard=keyboard.get_keyboard() if keyboard else None,
+            attachment=attachment
+        )
+    
+    def handle_new_message(self, event):
+        request = event.object.message['text']
+        user_id = event.object.message['from_id']
+        
+        if request == "Начать":
+            keyboard = VkKeyboard()
+            keyboard.add_button("Кнопка 1", color=VkKeyboardColor.SECONDARY)
+            keyboard.add_button("Кнопка 2", color=VkKeyboardColor.SECONDARY)
+            self.send_message(user_id, "Приветственное сообщение", keyboard)
+        elif request == "Кнопка 1":
+            self.send_profile(user_id)
+        elif request == "Кнопка 2":
+            self.send_favorites(user_id)
+    
+    def send_profile(self, user_id):
+        msg = 'Красотка Красивая'
+        link = 'https://vk.com/titronius'
+        attachment = "photo12908812_457240175,photo12908812_457240168"
+        
+        keyboard = VkKeyboard(inline=True)
+        keyboard.add_openlink_button(label='🔗 Ссылка на профиль', link=link)
+        keyboard.add_line()
+        keyboard.add_callback_button(label='❤️ Добавить в избранное', color=VkKeyboardColor.SECONDARY, payload={"type": "add_to_favorite"})
+        keyboard.add_callback_button(label='❌ Добавить в чс', color=VkKeyboardColor.SECONDARY, payload={"type": "add_to_blacklist"})
+        keyboard.add_line()
+        keyboard.add_callback_button(label='➡️ Следующий', color=VkKeyboardColor.SECONDARY, payload={"type": "next_people"})
+        
+        self.send_message(user_id, msg, keyboard, attachment)
+
+    def send_favorites(self, user_id):
+        # Логика вывода списка избранных людей.
+        pass
+
+    def handle_message_edit(self, event):
+        if event.object.payload.get('type') in self.callback_types:
+            self.vk.messages.sendMessageEventAnswer(
+                event_id=event.object.event_id,
+                user_id=event.object.user_id,
+                peer_id=event.object.peer_id,
+                event_data=json.dumps(event.object.payload)
+            )
         else:
-            print('хуяк')
-            
+            print('Unknown callback type')
+
+    def listen(self):
+        for event in self.longpoll.listen():
+            print(event)
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                if event.object.message['text'] != "":
+                    self.handle_new_message(event)
+            elif event.type == VkBotEventType.MESSAGE_EDIT:
+                self.handle_message_edit(event)
+
+if __name__ == "__main__":
+    # Загрузка переменных окружения из файла .env
+    load_dotenv(dotenv_path=Path('.').joinpath('.env'))
+
+    # Инициализация сессий VK API для группы и пользователя
+    vk_group_session = VKSession('VK_GROUP_TOKEN')
+    vk_user_session = VKSession('VK_USER_TOKEN')
+
+    # Подключение к базе данных
+    db_uri = get_env_variable('DB_URI')
+    engine = create_engine(db_uri)
+    create_tables(engine)  # Создание таблиц, если их нет
+
+    # Пример использования VKSession и User классов
+    user_id = 331709599
+
+    user = User(vk_user_session, user_id)
+    user_info = user.get_user_info()
+    print(f"User Info: {user_info}")
+
+    top_photos = user.get_top_photos()
+    print(f"Top Photos: {top_photos}")
+
+    search_params = {
+        'sex': 1,
+        'city': 'Оренбург',
+        'relation': 6,
+        'smoking': 0,
+        'alcohol': 0
+    }
+
+    user_ids = vk_user_session.search_users(search_params)
+    print(f"Найдено пользователей: {len(user_ids)}")
+    pprint(f"ID пользователей: {user_ids}")
+
+
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Инициализация бота и запуск обработки событий
+    group_id = int(get_env_variable('GROUP_ID'))
+    bot = VKBot(vk_group_session.vk_session, group_id=group_id)
+    bot.listen()
+
+    session.close()
